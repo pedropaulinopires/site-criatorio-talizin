6c94e32a89675384948ce050ad88c7aec150a064
Pedro Henrique Paulino Pires <121137670+pedropaulinopires@users.noreply.github.com>
2023-02-19 05:04:16 -0300

    menu mobile atualizado

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,6 +1,18 @@
-function openNav() {
-  document.getElementById("myNav").style.height = '100%'
+let menu = document.querySelector(".mobile-menu");
+
+let overlay = false;
+
+function clickMenu() {
+  if (overlay === false) {
+    //open menu
+    document.querySelector("#myNav").style.height = "100%";
+    menu.classList.toggle("active");
+    overlay = true;
+  } else {
+    //close menu
+    document.querySelector("#myNav").style.height = "0";
+    menu.classList.toggle("active");
+    overlay = false;
+  }
 }
-function closeNav() {
-    document.getElementById("myNav").style.height = '0'
-}
+
